510bdcf34f21c5725dff9ec4997bf30125129597
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-02-19 16:54:11 +0100

    Opzet voor de footer gemaakt

---
+++ view/navigatie/footer.php
@@ -0,0 +1,31 @@
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+          <!--Linkedin -->
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer text-center py-3">
+    Dol-fijn Tel: 06 23344639
+  </div>
+
+</footer>
