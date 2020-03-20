c61891f958d7e0e657d81e6281498ebca26f6c22
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-03-20 16:00:37 +0100

    cleared the footer

--- view/navigatie/footer.php
+++ view/navigatie/footer.php
@@ -1,31 +1,8 @@
-<!-- Footer -->
-<footer class="page-footer font-small cyan darken-3">
-
-  <!-- Footer Elements -->
-  <div class="container">
-
-    <!-- Grid row-->
-    <div class="row">
-
-      <!-- Grid column -->
-      <div class="col-md-12 py-5">
-        <div class="mb-5 flex-center">
-
-          <!-- Facebook -->
-          <a class="fb-ic">
-            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-          </a>
-          <!--Linkedin -->
-          <a class="li-ic">
-            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="footer text-center py-3">
+<footer class="page-footer font-small pt-2">
+  <div class="footer text-center">
+    <a id='facebookIcon' href="https://www.facebook.com/Gastouderopvang-Dol-fijn-van-klein-tot-groot-512381562119711/" taget='blank'>
+      <i class="fa fa-facebook-square fa-lg mr-md-5 mr-3 fa-2x"> </i>
+    </a>
     Dol-fijn Tel: 06 23344639
   </div>
-
 </footer>
