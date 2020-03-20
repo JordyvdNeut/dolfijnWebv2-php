78b6b5d0ee967ef1f21f81bc3ce4da223b746673
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-03-20 16:01:14 +0100

    turned it in a section

--- view/navigatie/header.php
+++ view/navigatie/header.php
@@ -1,6 +1,4 @@
-<section>
   <img class="col-12" src="<?=ROOT?>/assets/images/hero.png" />
-  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
     <?php require_once  "view/navigatie/nav.php"; ?>
   </nav>
-</section>
