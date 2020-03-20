d17beeea2f561f03fbf365f7557ece81423dc1b1
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-03-20 16:01:52 +0100

    styling

--- view/staatvoor/staatvoor.php
+++ view/staatvoor/staatvoor.php
@@ -2,13 +2,13 @@
 <?php
 require_once "view/navigatie/header.php";
 ?>
-<h1>Dol-Fijn Staat voor</h1>
+<h1 id='pageTitle'>Dol-Fijn staat voor</h1>
 
 <section class='col-12 row justify-content-around'>
 <?php
   $html = "";
   foreach($staanvoor as $staatvoor){
-    $html .= "<div class='col-5' id='staatvoor'>";
+    $html .= "<div class='col-5'>";
     $html .= "<h2>$staatvoor[title]</h2>";
     $html .= "<p>$staatvoor[text]</p>";
     $html .= "</div>";
