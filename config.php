b8550412da7f42ab78f48605f2c7523502eaecdf
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-04-14 09:22:28 +0200

    Made database name variable

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 <?php
-// define('ROOT', 'https://test.gastouderopvang-dol-fijn-nieuwerbrug.nl');
+// Local
 define('ROOT', 'http://localhost/gastouderopvang/dolfijnWebv2-php');
+define('databaseInfo', array("localhost", "mysql", "gastoude_websiteV2", "root", ""));
+
+// Server
+// define('ROOT', 'https://test.gastouderopvang-dol-fijn-nieuwerbrug.nl');
+// define('databaseInfo', array("https://cpanel1.cheesehosting.net:2083/", "mysql", "gastoude_websiteV2", "gastoude_jordy", "^Dol-Fijn34!"));
--- model/m.ThemasModel.php
+++ model/m.ThemasModel.php
@@ -1,12 +1,12 @@
 <?php
 require_once "model/m.Datahandler.php";
+require_once "config.php";
 
 class ThemasModel
 {
   public function __construct()
   {
-    $this->DataHandler = new DataHandler("localhost", "mysql", "gastoude_websiteV2", "root", "");
-    // $this->DataHandler = new DataHandler("https://cpanel1.cheesehosting.net:2083/", "mysql", "gastoude_websiteV2", "root", "");
+    $this->DataHandler = new DataHandler(databaseInfo[0], databaseInfo[1], databaseInfo[2], databaseInfo[3], databaseInfo[4]);
   }
 
 
