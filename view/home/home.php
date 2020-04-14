469db0860598d7253dad6c8e24ed9450bafe7dd7
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-04-14 09:45:59 +0200

    format

--- view/home/home.php
+++ view/home/home.php
@@ -5,21 +5,22 @@ require_once "view/navigatie/header.php";
 <h1 id='pageTitle'>Home</h1>
 
 <section class='col-12 row justify-content-around'>
-<div class='col-8' id='content-item'>
-  <h2>Waar voor staan we</h2>
-  <p>
-  Gastouderopvang Dol-Fijn staat voor:<br/>
-  <?php
-      // var_dump($subTitles->fetch(PDO::FETCH_ASSOC));
+  <div class='col-8' id='content-item'>
+    <h2>Waar voor staan we</h2>
+    <p>
+      Gastouderopvang Dol-Fijn staat voor:<br />
+      <?php
       $html = "";
-      $html .= "<ul>";
-    foreach($subTitles as $subTitle) {
-      $html .= "<li>".$subTitle['title'] . "</li>";
-    }
-      $html .= "</ul>";
+      foreach ($subTitles as $subTitle) {
+        $html .= "- " . $subTitle['title'] . "</br>";
+      }
       echo $html;
-  ?>
-  <a href='<?=ROOT?>/Staatvoor/staatvoorPage'><button class='btn btn-primary'>Bekijk meer</button></a>
-  </p>
-</div>
+      ?>
+      <a href='<?= ROOT ?>/Staatvoor/staatvoorPage'><button class='btn btn-primary'>Bekijk meer</button></a>
+    </p>
+  </div>
 </section>
+
+<?php
+require_once "view/navigatie/footer.php";
+?>
