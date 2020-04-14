147a124931dc0622ccb9eef8d770a9017a2137a2
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-04-14 09:46:40 +0200

    bugg fix

--- view/navigatie/footer.php
+++ view/navigatie/footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer font-small pt-2">
   <div class="footer text-center">
-    <a id='facebookIcon' href="https://www.facebook.com/Gastouderopvang-Dol-fijn-van-klein-tot-groot-512381562119711/" taget='blank'>
+    <a id='facebookIcon' href="https://www.facebook.com/Gastouderopvang-Dol-fijn-van-klein-tot-groot-512381562119711/" target='blank'>
       <i class="fa fa-facebook-square fa-lg mr-md-5 mr-3 fa-2x"> </i>
     </a>
     Dol-fijn Tel: 06 23344639
