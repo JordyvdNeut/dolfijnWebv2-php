dec9716b27e3161d25589719af6c7ea04044f900
JordyvdNeut-Jool <J.vanderneut@jool.nl>
2020-04-14 09:44:34 +0200

    Made a contact page

--- view/contact/contact.php
+++ view/contact/contact.php
@@ -2,4 +2,39 @@
   <?php
   require_once "view/navigatie/header.php";
   ?>
-  <h1>Contact</h1>
+
+  <h1 id="pageTitle">Contact</h1>
+  <section class='col-12 row justify-content-around'>
+    <div class='col-5' id='content-item'>
+      <h2>Contact gegevens</h2>
+      <p>
+        Anita van der Neut
+        </br>
+        Adres: Steenbakker 15
+        </br>
+        Plaats: Nieuwerbrug aan de rijn
+        </br>
+        Tel: 06 23344639
+        </br>
+        Mail: GastouderopvangDol-Fijn@hotmail.com
+        </br>
+        <i title="Landelijk Register Kinderopvang en Peuterspeelzalen" class="material-icons mat-icon">help</i><span title="Landelijk Register Kinderopvang en Peuterspeelzalen">LRKP: 315159479</span>
+        </br>
+        <i title="Kamer van koophandel" class="material-icons mat-icon">help</i><span title="Kamer van koophandel">KVK: 52481514</span>
+      </p>
+    </div>
+    <div class='col-5' id='content-item'>
+      <h2>Kennis making</h2>
+      <p>
+        Er is altijd ruimte voor overleg of een kennismakings gesprek.
+        De koffie, thee of limonade staat altijd klaar kom gerust eens langs.
+        </br></br>
+        Het is van belang dat uw kind het naar zijn zin heeft,
+        En een goed contact met de ouders is van groot belang.
+      </p>
+    </div>
+  </section>
+
+  <?php
+  require_once "view/navigatie/footer.php";
+  ?>
